52ba3069d4832992fe8be8c02e0dd1c5207c6b91
Victor Ay <viktoray007@gmail.com>
2019-10-01 12:49:13 +0300

    fix link

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open('README.md', 'r') as f:
     long_description = f.read()
 
 setuptools.setup(
-     name='send_eth',
+     name='send_eth_run',
      version='1.0',
-     scripts=['send_eth'] ,
+     scripts=['send_eth_run'] ,
      author="Victor A",
      author_email="viktoray007@gmail.com",
      description="An ethereum mass sending package",
      long_description=long_description,
      long_description_content_type="text/markdown",
-     url="https://github.com/victoray",
+     url="https://github.com/victoray/send_eth/",
      packages=setuptools.find_packages(),
      classifiers=[
          "Programming Language :: Python :: 3",
